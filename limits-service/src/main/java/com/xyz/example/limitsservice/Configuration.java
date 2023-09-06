683f3bd46afb381bf4e27cb0b6a08fdbd554c55c
MohamedAzarudeen <azartechnet@gmail.com>
2023-09-06 12:12:55 +0530

    updated

--- limits-service/src/main/java/com/xyz/example/limitsservice/Configuration.java
+++ limits-service/src/main/java/com/xyz/example/limitsservice/Configuration.java
@@ -1,35 +1,26 @@
 package com.xyz.example.limitsservice;
 
-public class LimitConfiguration {
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("limits-service")
+public class Configuration {
 
 	private int maximum;
 	private int minimum;
-	
-	protected LimitConfiguration()
-	{
-		
-	}
-
 	public int getMaximum() {
 		return maximum;
 	}
-
 	public void setMaximum(int maximum) {
 		this.maximum = maximum;
 	}
-
 	public int getMinimum() {
 		return minimum;
 	}
-
 	public void setMinimum(int minimum) {
 		this.minimum = minimum;
 	}
-
-	public LimitConfiguration(int maximum, int minimum) {
-		super();
-		this.maximum = maximum;
-		this.minimum = minimum;
-	}
+	
 	
 }
--- limits-service/src/main/java/com/xyz/example/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/xyz/example/limitsservice/LimitsConfigurationController.java
@@ -1,14 +1,20 @@
 package com.xyz.example.limitsservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.xyz.example.limitsservice.bean.LimitConfiguration;
 
 @RestController
 public class LimitsConfigurationController {
 
+	@Autowired
+	private Configuration config;
 	@GetMapping("/limits")
 	public LimitConfiguration retriveLimitsFromConfigurations()
 	{
-		return new LimitConfiguration(1000,1);
+		//return new LimitConfiguration(1000,1);
+		return new LimitConfiguration(config.getMaximum(),config.getMinimum());
 	}
 }
