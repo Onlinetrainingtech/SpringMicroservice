bb6b7a9f11005edd788bf00d3dc60c80c01d2008
azartechnet <azartechnet@gmail.com>
2024-08-21 16:52:45 +0530

    updated

---
+++ limits-serviceP3/src/main/java/com/example/limits_serviceP3/LimitsServiceP3Application.java
@@ -0,0 +1,13 @@
+package com.example.limits_serviceP3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitsServiceP3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitsServiceP3Application.class, args);
+	}
+
+}
