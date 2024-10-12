61a255452d24e5d4b165be7a46ba1c1c3315e28d
azartechnet <azartechnet@gmail.com>
2024-10-12 16:11:07 +0530

    updated

---
+++ BSpring-cloud-config-server/src/test/java/com/example/BSpring_cloud_config_server/BSpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BSpring_cloud_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BSpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- limits-serviceP5/src/main/java/com/example/limits_serviceP5/LimitsConfigurationController.java
+++ limits-serviceP5/src/main/java/com/example/limits_serviceP5/LimitsConfigurationController.java
@@ -1,5 +1,6 @@
 package com.example.limits_serviceP5;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LimitsConfigurationController {
 
-	@GetMapping
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping(value="/limits")
 	public LimitsConfiguration retriveLimitsFromConfigurations()
 	{
-		return new LimitsConfiguration(1000,1);
+		return new LimitsConfiguration(configuration.getMaximum(),configuration.getMinimum());
 	}
 }
